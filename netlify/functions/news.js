51a0277b2f750dd591ba90a8a7c2e8c212270782
riranrmarques-ctrl <riran.rmarques@gmail.com>
2026-04-09 05:15:31 -0300

    news.js

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,50 +1,73 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
+<div class="qode-news-list-inner-holder qode-slider1-owl qode-owl-slider-style owl-loaded owl-drag" data-number-of-items="1" style="visibility: visible;">
 
-exports.handler = async function () {
-  const res = await fetch("https://ilheuseventos.com.br/");
-  const html = await res.text();
 
-  const $ = cheerio.load(html);
-
-  let tituloPrincipal = "";
-  let imagemPrincipal = "";
-
-  // Pega qualquer título válido (mais seguro)
-  $("a").each((i, el) => {
-    const texto = $(el).text().trim();
-
-    if (
-      texto.length > 30 && 
-      texto.length < 200 &&
-      !tituloPrincipal
-    ) {
-      tituloPrincipal = texto;
-    }
-  });
-
-  // Pega primeira imagem real (ignora base64)
-  $("img").each((i, el) => {
-    const src =
-      $(el).attr("data-src") ||
-      $(el).attr("data-lazy-src") ||
-      $(el).attr("src");
-
-    if (src && !src.startsWith("data:image") && !imagemPrincipal) {
-      imagemPrincipal = src;
-    }
-  });
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify([
-      {
-        titulo: tituloPrincipal,
-        imagem: imagemPrincipal
-      }
-    ])
-  };
-};
+<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all; width: 4050px;"><div class="owl-item active" style="width: 1350px;"><div class="qode-news-item qode-slider1-item qode-slider-size-landscape" data-thumb-url="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5912-150x150.jpeg" data-date="15 horas atrás">
+	<div class="qode-news-item-image-holder lazyloaded" style="background-image: url(&quot;https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5912.jpeg&quot;);" data-bg-image="url(https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5912.jpeg)"></div>
+	<div class="qode-ni-content">
+					<div class="container_inner">
+						<div class="qode-ni-content-table">
+					<div class="qode-ni-content-table-cell">
+													<div class="qode-ni-info-top-holder">
+								<div class="qode-ni-info qode-ni-info-top">
+										<div class="qode-post-info-category">
+		<a href="https://ilheuseventos.com.br/category/noticias/ilheus/"><span class="qode-category-name" style="background-color:#dd3333;">Ilhéus </span></a>	</div>
+								</div>
+							</div>
+													<div class="qode-ni-title-holder">
+								
+<h1 itemprop="name" class="entry-title qode-post-title">
+    <a itemprop="url" href="https://ilheuseventos.com.br/noticias/ilheus/moradores-denunciam-conduta-de-motoristas-da-atlantico-em-linha-da-zona-norte-de-ilheus/" title="Moradores denunciam conduta de motoristas da Atlântico em linha da zona norte de Ilhéus">
+        Moradores denunciam conduta de motoristas da Atlântico em linha da zona norte de Ilhéus    </a>
+</h1>							</div>
+																							</div>
+				</div>
+					</div>
+			</div>
+</div></div><div class="owl-item" style="width: 1350px;"><div class="qode-news-item qode-slider1-item qode-slider-size-landscape" data-thumb-url="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5901-150x150.jpeg" data-date="16 horas atrás">
+	<div class="qode-news-item-image-holder lazyloaded" style="background-image: url(&quot;https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5901.jpeg&quot;);" data-bg-image="url(https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5901.jpeg)"></div>
+	<div class="qode-ni-content">
+					<div class="container_inner">
+						<div class="qode-ni-content-table">
+					<div class="qode-ni-content-table-cell">
+													<div class="qode-ni-info-top-holder">
+								<div class="qode-ni-info qode-ni-info-top">
+										<div class="qode-post-info-category">
+		<a href="https://ilheuseventos.com.br/category/entretenimento/"><span class="qode-category-name" style="background-color:#8224e3;">Entretenimento </span></a><a href="https://ilheuseventos.com.br/category/noticias/ilheus/"><span class="qode-category-name" style="background-color:#dd3333;">Ilhéus </span></a>	</div>
+								</div>
+							</div>
+													<div class="qode-ni-title-holder">
+								
+<h1 itemprop="name" class="entry-title qode-post-title">
+    <a itemprop="url" href="https://ilheuseventos.com.br/noticias/ilheus/whindersson-esgota-sessao-das-20h-e-abre-novo-show-as-17h-em-ilheus/" title="Whindersson esgota sessão das 20h e abre novo show às 17h em Ilhéus">
+        Whindersson esgota sessão das 20h e abre novo show às 17h em Ilhéus    </a>
+</h1>							</div>
+																							</div>
+				</div>
+					</div>
+			</div>
+</div></div><div class="owl-item" style="width: 1350px;"><div class="qode-news-item qode-slider1-item qode-slider-size-landscape" data-thumb-url="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/WhatsApp-Image-2026-04-08-at-11.36.56-e1775659052782-150x150.jpeg" data-date="17 horas atrás">
+	<div class="qode-news-item-image-holder lazyload" style="background-image:inherit" data-bg-image="url(https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/WhatsApp-Image-2026-04-08-at-11.36.56-e1775659052782.jpeg)"></div>
+	<div class="qode-ni-content">
+					<div class="container_inner">
+						<div class="qode-ni-content-table">
+					<div class="qode-ni-content-table-cell">
+													<div class="qode-ni-info-top-holder">
+								<div class="qode-ni-info qode-ni-info-top">
+										<div class="qode-post-info-category">
+		<a href="https://ilheuseventos.com.br/category/noticias/ilheus/"><span class="qode-category-name" style="background-color:#dd3333;">Ilhéus </span></a>	</div>
+								</div>
+							</div>
+													<div class="qode-ni-title-holder">
+								
+<h1 itemprop="name" class="entry-title qode-post-title">
+    <a itemprop="url" href="https://ilheuseventos.com.br/noticias/ilheus/imagem-mostra-mulher-em-situacao-de-rua-deitada-em-via-publica-e-acende-alerta-em-ilheus/" title="Imagem mostra mulher em situação de rua deitada em via pública e acende alerta em Ilhéus">
+        Imagem mostra mulher em situação de rua deitada em via pública e acende alerta em Ilhéus    </a>
+</h1>							</div>
+																							</div>
+				</div>
+					</div>
+			</div>
+</div></div></div></div><div class="owl-nav disabled"><div class="owl-prev"><span class="qode-prev-icon ion-ios-arrow-left"></span></div><div class="owl-next"><span class="qode-next-icon ion-ios-arrow-right"></span></div></div><div class="qode-slider1-nav-holder"><div class="container_inner"><div class="qode-slider-nav-holder-inner"><div class="qode-slider-nav-holder-inner2"><div class="owl-dots" style="overflow-y: hidden; outline: none;" tabindex="0"><div class="owl-dot active"><div class="qode-slider1-pag-thumb"><img alt="thumb" src="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5912-150x150.jpeg"></div><div class="qode-slider1-pag-info-holder"><h5 class="qode-slider1-pag-title">
+        Moradores denunciam conduta de motoristas da Atlântico em linha da zona norte de Ilhéus    </h5><div class="qode-slider1-pag-date"><i class="dripicons-alarm"></i>15 horas atrás</div></div></div><div class="owl-dot"><div class="qode-slider1-pag-thumb"><img alt="thumb" src="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/IMG_5901-150x150.jpeg"></div><div class="qode-slider1-pag-info-holder"><h5 class="qode-slider1-pag-title">
+        Whindersson esgota sessão das 20h e abre novo show às 17h em Ilhéus    </h5><div class="qode-slider1-pag-date"><i class="dripicons-alarm"></i>16 horas atrás</div></div></div><div class="owl-dot"><div class="qode-slider1-pag-thumb"><img alt="thumb" src="https://ilheuseventos.com.br/site/wp-content/uploads/2026/04/WhatsApp-Image-2026-04-08-at-11.36.56-e1775659052782-150x150.jpeg"></div><div class="qode-slider1-pag-info-holder"><h5 class="qode-slider1-pag-title">
+        Imagem mostra mulher em situação de rua deitada em via pública e acende alerta em Ilhéus    </h5><div class="qode-slider1-pag-date"><i class="dripicons-alarm"></i>17 horas atrás</div></div></div></div></div></div></div><div id="ascrail2000" class="nicescroll-rails nicescroll-rails-vr" style="width: 2px; z-index: 5; background: rgba(255, 255, 255, 0.5); cursor: default; position: absolute; top: 200.305px; left: 1650.5px; height: 450.516px; opacity: 1; display: none;"><div class="nicescroll-cursors" style="position: relative; top: 0px; float: right; width: 2px; height: 0px; background-color: rgb(255, 255, 255); border: 0px; background-clip: padding-box; border-radius: 0px;"></div></div></div></div>
